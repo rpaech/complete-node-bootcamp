908b0b3b3a6272e9e2a021b600479222e7dc01aa
Robert Paech <robert.paech@gmail.com>
2023-08-10 10:56:41 +1000

    Lecture 56

--- 4-natours/working/app.js
+++ 4-natours/working/app.js
@@ -1,16 +1,63 @@
-import express from "express";
+import fs from "fs";
+import express, { response } from "express";
+import { __dirname, __filename } from "./helpers.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  // response.status(200).send("Hello from the server side!");
+const toursDataFile = `${__dirname}/dev-data/data/tours-working.json`;
+const toursApiPath = "/api/v1/tours";
+
+const tours = JSON.parse(fs.readFileSync(toursDataFile));
+
+app.get(toursApiPath, (request, response) =>
   response
     .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours " });
+    .json({ status: "success", results: tours.length, data: { tours } })
+);
+
+app.get(`${toursApiPath}/:id`, (request, response) => {
+  const id = Number(request.params.id);
+  const tour = tours.find((entry) => entry.id === id);
+
+  if (!tour) return response.status(404).json({ status: "fail" });
+
+  response.status(200).json({ status: "success", data: { tour } });
 });
 
-app.post("/", (request, response) => {
-  response.send("You can post to this endpoint!");
+app.post(toursApiPath, (request, response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, request.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(toursDataFile, JSON.stringify(tours), (error) =>
+    response.status(200).json({ status: "success", data: { tour: newTour } })
+  );
+});
+
+app.patch(`${toursApiPath}/:id`, (request, response) => {
+  const id = Number(request.params.id);
+  const tour = tours.find((entry) => entry.id === id);
+
+  if (!tour) return response.status(404).json({ status: "fail" });
+
+  // TODO: Update an existing tour.
+
+  response
+    .status(200)
+    .json({ status: "success", data: { tour: "<UPDATED TOUR GOES HERE>" } });
+});
+
+app.delete(`${toursApiPath}/:id`, (request, response) => {
+  const id = Number(request.params.id);
+  const tour = tours.find((entry) => entry.id === id);
+
+  // TODO: Delete an existing tour.
+
+  if (!tour) return response.status(404).json({ status: "fail" });
+
+  response.status(204).json({ status: "success", data: null });
 });
 
 const port = 3000;
